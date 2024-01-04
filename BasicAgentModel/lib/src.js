daa86ffc76bf6a2c04f6794360d1ec562aff6625
becauselol <gabesytf@gmail.com>
2024-01-04 15:46:14 +0800

    feat: add import into src.js

--- BasicAgentModel/lib/src.js
+++ BasicAgentModel/lib/src.js
@@ -1,7 +1,7 @@
 // First we define some general parameters for our simulation
 var fps = 10; // frames per real time second 
 // FPS needs to be at least 5 and all waitTimes of trains and edge weights must be at least 1
-var timestep = 1/fps;
+var timestep = 1 / fps;
 
 // The probability of a patient arrival needs to be less than the probability of a departure, else an infinite queue will build.
 // You also need to allow travel time for patients to move from their seat in the waiting room to get close to the doctor.
@@ -14,14 +14,14 @@ var probDeparture = 0.4;
 // Later assignments can build on this basic structure.
 var probTypeA = 0.5;
 
-const sim = Simulation()
+const sim = new Simulation()
 
 function init() {
-	// Your page initialization code goes here
-	// All elements of the DOM will be available here
-	window.addEventListener("resize", redrawWindow); //Redraw whenever the window is resized
-	simTimer = window.setInterval(sim.simStep, timestep); // call the function simStep every animationDelay milliseconds
-	redrawWindow();
+  // Your page initialization code goes here
+  // All elements of the DOM will be available here
+  window.addEventListener("resize", redrawWindow); //Redraw whenever the window is resized
+  simTimer = window.setInterval(sim.simStep, timestep); // call the function simStep every animationDelay milliseconds
+  redrawWindow();
 }
 
 init();
